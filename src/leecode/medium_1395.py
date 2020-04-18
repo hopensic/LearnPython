da09e86b8adb3e0826da7ee4f722396fb26838be
hopensic <hopensic@gmail.com>
2020-04-19 00:22:10 +0800

    leetcode 1395 tag: ^1395 ^medium ^array name: ^(Count Number of Teams) '''

--- src/leecode/medium_1395.py
+++ src/leecode/medium_1395.py
@@ -9,22 +9,23 @@ name: ^(Count Number of Teams)
 class Solution:
     def numTeams(self, rating: list) -> int:
         le = len(rating)
-        if le<3:
+        if le < 3:
             return 0
+        res = 0
+        for i in range(le):
+            for j in range(i + 1, le):
+                for k in range(j + 1, le):
+                    if (rating[i] < rating[j] < rating[k] or
+                            rating[i] > rating[j] > rating[k]):
+                        res += 1
 
-
-        increase_lst, decrease_lst = [[rating[0]]], [[rating[0]]]
-        i_idx, d_idx = 0, 0
-        for i in range(len(rating)-1):
-
-
-        return
+        return res
 
 
 arr1 = [1, 3, 2, 9, 7, 5, 6]
 
 t1 = datetime.now()
 s = Solution()
-print(s.findSpecialInteger(arr1))
+print(s.numTeams(arr1))
 t2 = datetime.now()
 print(t2 - t1)
