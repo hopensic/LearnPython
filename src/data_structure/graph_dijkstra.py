095f6c02733b5276dc4d8a038e1dc2d832ea53cd
hopensic <hopensic@gmail.com>
2020-06-16 23:32:21 +0800

    algorithm graph dijkstra

---
+++ src/data_structure/graph_dijkstra.py
@@ -0,0 +1,49 @@
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"] = {}
+graph["a"]["fin"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {}
+
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = set()
+
+
+def find_lowest_cost_node(costs: dict):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node, cost in costs.items():
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
+# 找到开销最小的节点
+node = find_lowest_cost_node(costs)
+while node:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if new_cost < costs[n]:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.add(node)
+    node = find_lowest_cost_node(costs)
+
+print(1)
