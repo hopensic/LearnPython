2f8a1af3634008b5f6052ffb2823b122d9150fc3
Oliver Wong <hopensic@gmail.com>
2025-02-28 19:29:41 +0800

    delete some files

--- src/python_basic_course/ch03.str/e1.py
+++
@@ -1,4 +0,0 @@
-format = "Hello, %s. %s enough for fa?"
-values = ('world', 'Hot')
-d = format % values
-print(d)
--- src/python_basic_course/ch03.str/e2.py
+++
@@ -1,5 +0,0 @@
-from string import Template
-
-tmpl = Template("Hello ,$who! $what enough for ya?")
-d = tmpl.substitute(who="Mars", what="Dusty")
-print(d)
